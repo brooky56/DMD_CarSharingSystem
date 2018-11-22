02877102446437371260953fb49605896dc7f083
Aidar <brooklyn56@yandex.ru>
2018-11-22 03:37:07 +0300

    Improvement

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,5 +1,6 @@
 package gui;
 
+import db.Predefined;
 import db.SQLQuery;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
@@ -7,15 +8,46 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 import main.Common;
 import main.Table;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Controller {
+
+    @FXML
+    private CheckBox updateQueryCheckBox;
+
+    @FXML
+    private HBox CustomFieldHBox;
+
+    @FXML
+    private Label predefinedQueryLabel;
+
+    @FXML
+    private VBox tableListVBox;
+
+    @FXML
+    private Label tableListLabel;
+
+    @FXML
+    private VBox CustomVBox;
+
+    @FXML
+    private Button predefinedResultButton;
+
+    @FXML
+    private VBox predefinedQueryVBox;
+
+    @FXML
+    private TextArea textAreaForInput;
+
+    @FXML
+    private ComboBox<String> predefinedQueryBox;
 
     @FXML
     private Button buttonShowTable;
@@ -35,7 +67,7 @@ public class Controller {
     @FXML
     private TableView<ObservableList> table;
 
-    private void initData() throws SQLException {
+    private void fullFillTableList() throws SQLException {
         ObservableList<String> tableList = FXCollections.observableArrayList();
         ResultSet tableSet = main.Common.connection().createStatement().executeQuery("SELECT name FROM sqlite_master WHERE type = 'table' AND name <> 'sqlite_master' AND name <> 'sqlite_sequence'");
         while (tableSet.next()) {
@@ -47,12 +79,34 @@ public class Controller {
         dbTableBox.setItems(tableList);
     }
 
+    private void fullFillPredefinedQueryList() {
+        ObservableList<String> queryList = FXCollections.observableArrayList();
+        for (int i = 1; i < 11; i++) {
+            queryList.add("Query " + i);
+        }
+        predefinedQueryBox.setItems(queryList);
+    }
+
+    private void initData() throws SQLException {
+        fullFillTableList();
+        fullFillPredefinedQueryList();
+    }
+
     @FXML
     private void handleTextFieldAction() {
         if (!queryField.getText().equals("")) {
             executeButton.setDisable(false);
         } else {
             executeButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    private void handlePredefinedComboBoxAction() {
+        if (!predefinedQueryBox.getValue().equals("")) {
+            predefinedResultButton.setDisable(false);
+        } else {
+            predefinedResultButton.setDisable(true);
         }
     }
 
@@ -74,6 +128,30 @@ public class Controller {
         buildTable(selectQuery);
     }
 
+    @FXML
+    private void onButtonShowQueryResultClick() {
+        clear();
+        String[] inputLine;
+        String numberOfQuery = predefinedQueryBox.getValue();
+
+        if (!textAreaForInput.getText().equals("")) {
+            inputLine = textAreaForInput.getText().split("\\n");
+        } else {
+            inputLine = null;
+        }
+
+        Table t = null;
+        switch (numberOfQuery) {
+            case "Query 1":
+                t = Predefined.findCar(inputLine[0], inputLine[1]);
+                break;
+            case "Query 2":
+                t = Predefined.socketsPerHour(inputLine[0]);
+                break;
+        }
+        buildPredefinedQuery(t);
+    }
+
     private void clear() {
         for (int i = 0; i < table.getItems().size(); i++) {
             table.getItems().clear();
@@ -85,12 +163,16 @@ public class Controller {
     private void onButtonExecuteQueryClick() {
         clear();
         String query = queryField.getText();
-        if (query != null) {
-            System.out.println(query);
+        boolean f = updateQueryCheckBox.isSelected();
+        System.out.println(f);
+        if (query != null && !f) {
+            System.out.println(query + " " + f);
             buildTable(query);
-        } else {
+        } else if (query != null && f) {
+            System.out.println(query + " " + f);
+            SQLQuery.executeQueryNoOutput(query);
+        } else
             executeButton.setDisable(false);
-        }
     }
 
     @FXML
@@ -99,13 +181,14 @@ public class Controller {
         table.setEditable(false);
         executeButton.setDisable(true);
         buttonShowTable.setDisable(true);
+        predefinedResultButton.setDisable(true);
     }
 
     private ObservableList<ObservableList> data;
     private ObservableList<String> row;
     private TableColumn col;
 
-    public void buildTable(String SQL) {
+    private void buildTable(String SQL) {
         Table t = SQLQuery.executeQueryWithOutput(SQL);
         data = FXCollections.observableArrayList();
         for (int i = 0; i < t.width; i++) {
@@ -131,42 +214,28 @@ public class Controller {
         table.setItems(data);
     }
 
-    public void buildData(String SQL) {
-        Connection c;
-
+    private void buildPredefinedQuery(Table t) {
         data = FXCollections.observableArrayList();
-        try {
-            c = Common.connection();
-            //ResultSet
-            ResultSet rs = c.createStatement().executeQuery(SQL);
-
-            //TABLE COLUMN ADDED DYNAMICALLY
-            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
-                //We are using non property style for making dynamic table
-                final int j = i;
-                col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
-                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
-                table.getColumns().addAll(col);
-                System.out.println("Column [" + i + "] ");
-            }
-
-            // Data added to ObservableList
-            while (rs.next()) {
-                //Iterate Row
-                row = FXCollections.observableArrayList();
-                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                    //Iterate Column
-                    row.add(rs.getString(i));
-                }
-                System.out.println("Row [1] added " + row);
-                data.add(row);
-            }
-
-            //FINALLY ADDED TO TableView
-            table.setItems(data);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error on Building Data");
+        for (int i = 0; i < t.width; i++) {
+            col = new TableColumn(t.getTitle(i).toString());
+            final int j = i;
+            col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+            table.getColumns().add(col);
+            System.out.println("Column [" + i + "] ");
         }
+
+        for (int i = 1; i < t.height; i++) {
+            row = FXCollections.observableArrayList();
+            for (int j = 0; j < t.width; j++) {
+                if (t.getCell(i, j) == null) {
+                    row.add(Common.NULL_ELEMENT);
+                } else {
+                    row.add(t.getCell(i, j).toString());
+                }
+            }
+            System.out.println("Row [" + i + "] added " + row);
+            data.add(row);
+        }
+        table.setItems(data);
     }
 }
--- src/gui/StartRun.java
+++ src/gui/StartRun.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import main.Common;
 
 import java.io.IOException;
+
 
 public class StartRun extends Application {
 
